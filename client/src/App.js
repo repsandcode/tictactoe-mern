358febd842497fa587ab51b50b08b9edd21fb8b3
repsandcode <albertjonathan.dev@gmail.com>
2023-07-19 21:55:54 +0800

    add react-router

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,22 @@
 // import React, {useState} from 'react';
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import './App.css';
-
-import GamePage from './pages/gamePage/index';
+import "./App.css";
+import GamePage from "./pages/gamePage/index";
 
 function App() {
-
   return (
     <div className="App">
-      <GamePage/>
+      <GamePage />
     </div>
   );
 }
 
 export default App;
+
+// import Layout from "./pages/Layout";
+// import Home from "./pages/Home";
+// import Blogs from "./pages/Blogs";
+// import Contact from "./pages/Contact";
+// import NoPage from "./pages/NoPage";
